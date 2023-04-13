8f59b56817153cac4964d68ed2844586ca8b402c
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2023-04-13 15:31:28 +0300

    готовый 12 проект после правок ревью

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react"
-import { Switch, Route, Link } from "react-router-dom"
+import { useLocation, Link } from "react-router-dom"
 import MobileMenu from "./MobileMenu"
 import burgerMenu from "../images/Line.svg"
 import closeMenuIcon from "../images/close-icon-mobile.svg"
 
 function Header(props) {
+  const location = useLocation()
   return (
     <div>
       <MobileMenu
@@ -14,30 +15,30 @@ function Header(props) {
       />
       <header className="header page__header mobile-menu__header">
         <a className="logo" href="#" target="_self" />
-        <Switch>
-          <Route exact path="/sign-in">
-            <Link to="/sign-up" className="header__link">
-              Регистрация
+
+        {location.pathname === "/sign-in" && (
+          <Link to="/sign-up" className="header__link">
+            Регистрация
+          </Link>
+        )}
+        {location.pathname === "/sign-up" && (
+          <Link to="/sign-in" className="header__link">
+            Войти
+          </Link>
+        )}
+        {location.pathname === "/" && (
+          <div className="header__user-info">
+            <p className="header__email">{props.email}</p>
+            <Link
+              to="/sign-in"
+              className="header__link"
+              onClick={props.onSignOut}
+            >
+              Выйти
             </Link>
-          </Route>
-          <Route exact path="/sign-up">
-            <Link to="/sign-in" className="header__link">
-              Войти
-            </Link>
-          </Route>
-          <Route exact path="/">
-            <div className="header__user-info">
-              <p className="header__email">{props.email}</p>
-              <Link
-                to="/sign-in"
-                className="header__link"
-                onClick={props.onSignOut}
-              >
-                Выйти
-              </Link>
-            </div>
-          </Route>
-        </Switch>
+          </div>
+        )}
+
         {props.isLoggedIn && (
           <button
             className="header__burger"
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,18 @@
-import React from "react";
+import AuthForm from "./AuthForm"
 
 function Login(props) {
-  const [email, setEmail] = React.useState("");
-  const [password, setpassword] = React.useState("");
-
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
-  
-  function handlePasswordChange(e) {
-    setpassword(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    props.onLogin(email, password);
+  function handleSubmit(email, password) {
+    props.onLogin(email, password)
   }
 
   return (
-    <>      
-      <section className="auth">
-        <h2 className="auth__title">Вход</h2>
-        <form className="auth__form" onSubmit={handleSubmit}>
-          <input
-            onChange={handleEmailChange}
-            className="auth__form-input"
-            placeholder="Email"
-            name="email"
-            type="email"
-            required
-            value={email || ""}
-            autoComplete="off"
-          ></input>
-          <input
-            onChange={handlePasswordChange}
-            className="auth__form-input"
-            placeholder="Пароль"
-            name="password"
-            type="password"
-            required
-            value={password || ""}
-            autoComplete="off"
-          ></input>
-
-          <button className="auth__form-submit-btn" type="submit">
-            Войти
-          </button>
-        </form>
-      </section>
-    </>
-  );
+    <AuthForm
+      title="Вход"
+      buttonText="Войти"
+      onSubmit={handleSubmit}
+      isSubmitting={props.isSubmitting}
+    />
+  )
 }
 
-export default Login;
+export default Login
